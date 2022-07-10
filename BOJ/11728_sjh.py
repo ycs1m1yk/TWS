76147f16f334115f255dbee65b651e0c64adb07c
Jaehyeok <milkboy2564@naver.com>
2022-07-10 22:33:07 +0900

    220710 - 배열 합치기

---
+++ BOJ/11728_sjh.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.extend(b)
+print(*sorted(a))
