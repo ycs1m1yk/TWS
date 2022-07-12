347430ff7062d0b4a24fb9a338c3b00761a678ec
ycs1m1yk <satoly4@gmail.com>
2022-07-12 12:24:27 +0900

    220712 - 두 용액(누더기)

---
+++ BOJ/2470_lhs.js
@@ -0,0 +1,92 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+const stdin = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(/\r\n|\n/);
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const MAX = 2 * 10 ** 9;
+
+const lowerBound = (target, arr, lo, hi) => {
+  while (lo + 1 < hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (target > arr[mid]) lo = mid;
+    else hi = mid;
+  }
+
+  return hi;
+};
+
+const closest = (target, arr, lb) => {
+  const sums = [
+    arr[lb - 1] !== target ? Math.abs(arr[lb - 1] + target) : MAX,
+    arr[lb] ? Math.abs(arr[lb] + target) : MAX,
+    arr[lb + 1] ? Math.abs(arr[lb + 1] + target) : MAX,
+  ];
+  const minIdx = sums.findIndex((v) => v === Math.min(...sums));
+  return { sumAbs: sums[minIdx], offset: minIdx - 1 };
+};
+
+const solve = (arr) => {
+  /**
+   * Case
+   * 1: 산성만
+   * 2: 알칼리만
+   * 3: 혼합
+   */
+  const candidates = {
+    case1: { sumAbs: MAX, str: "" },
+    case2: { sumAbs: MAX, str: "" },
+    case3: { sumAbs: MAX, str: "" },
+  };
+
+  const zeroLb = lowerBound(0, arr, -1, arr.length);
+  if (zeroLb <= arr.length - 2) {
+    candidates["case1"].sumAbs = arr[zeroLb] + arr[zeroLb + 1];
+    candidates["case1"].str = arr[zeroLb] + " " + arr[zeroLb + 1];
+  }
+  if (zeroLb >= 2) {
+    candidates["case2"].sumAbs = -arr[zeroLb - 2] + -arr[zeroLb - 1];
+    candidates["case2"].str = arr[zeroLb - 2] + " " + arr[zeroLb - 1];
+  }
+
+  let minAbs = MAX;
+  if (zeroLb !== 0 && zeroLb !== arr.length) {
+    for (let i = 0; i < zeroLb; i++) {
+      const curr = arr[i];
+      const lb = lowerBound(-curr, arr, zeroLb - 1, arr.length);
+      const { sumAbs, offset } = closest(curr, arr, lb);
+
+      if (sumAbs < minAbs) {
+        candidates["case3"].sumAbs = sumAbs;
+        candidates["case3"].str = curr + " " + arr[lb + offset];
+        minAbs = sumAbs;
+      }
+    }
+  }
+
+  let pick = "case3";
+  if (candidates["case1"].sumAbs < minAbs) {
+    pick = "case1";
+    minAbs = candidates["case1"].sumAbs;
+  }
+  if (candidates["case2"].sumAbs < minAbs) pick = "case2";
+
+  return candidates[pick].str;
+};
+
+function main() {
+  const N = +input();
+  const values = input()
+    .split(" ")
+    .map(Number)
+    .sort((l, r) => l - r);
+
+  console.log(solve(values));
+}
+
+main();
