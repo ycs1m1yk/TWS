8e992100d248d001eb478d37b9e527fe819afbe4
ycs1m1yk <satoly4@gmail.com>
2022-08-05 23:38:57 +0900

    220805-줄어드는 수
    - BFS
    - cnt === N 일때까지만 탐색

--- BOJ/1174_lhs.js
+++ BOJ/1174_lhs.js
@@ -2,23 +2,34 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
 const N = +require("fs").readFileSync(filePath).toString().trim();
 
 function main() {
+  if (N <= 10) {
+    console.log(N - 1);
+    return;
+  }
   if (N > 1023) {
     console.log(-1);
     return;
   }
 
-  const result = [];
-  function dfs(curr) {
-    result.push(+curr);
-    for (let next = 0; next < +curr.at(-1); next++) {
-      dfs(curr + next + "");
+  const q = [...Array(10)].map((_, i) => i);
+  let [cnt, ans] = [10, 0];
+  while (q.length) {
+    if (cnt === N) {
+      console.log(ans);
+      break;
+    }
+    const front = q.shift();
+
+    for (let i = 0; i < front % 10; i += 1) {
+      cnt += 1;
+      const num = front * 10 + i;
+      q.push(num);
+      if (cnt === N) {
+        ans = num;
+        break;
+      }
     }
   }
-  for (let i = 0; i <= 9; i++) {
-    dfs(i + "");
-  }
-  result.sort((l, r) => l - r);
-  console.log(result[N - 1]);
 }
 
 main();
