42b337aee3863c93cdb46bc7fb0f4e6bb5694142
ycs1m1yk <satoly4@gmail.com>
2022-08-08 17:02:53 +0900

    220808-오픈채팅방

---
+++ Programmers/42888_lhs.js
@@ -0,0 +1,27 @@
+function solution(record) {
+  const answer = [];
+  const idMap = {};
+  record.forEach((el) => {
+    const [action, id, name] = el.split(" ");
+    switch (action) {
+      case "Enter":
+        idMap[id] = name;
+        answer.push(`${id}님이 들어왔습니다.`);
+        break;
+      case "Leave":
+        answer.push(`${id}님이 나갔습니다.`);
+        break;
+      case "Change":
+        idMap[id] = name;
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return answer.map((message) => {
+    const id = message.split("님")[0];
+    return message.replace(id, idMap[id]);
+  });
+}
