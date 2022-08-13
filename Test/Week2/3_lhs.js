5e2293c2ee74d28da8a330d9a1d59d047b471a08
ycs1m1yk <satoly4@gmail.com>
2022-08-13 22:37:16 +0900

    220813 - 3번 업솔빙

--- Test/Week2/3_lhs.js
+++ Test/Week2/3_lhs.js
@@ -1,3 +1,77 @@
+/**
+  테스트 1 〉 통과 (0.45ms, 30.2MB)
+  테스트 2 〉 통과 (0.42ms, 30.2MB)
+  테스트 3 〉 통과 (0.44ms, 30.2MB)
+  테스트 4 〉 통과 (0.16ms, 29.9MB)
+  테스트 5 〉 통과 (0.53ms, 30.1MB)
+  테스트 6 〉 통과 (75.31ms, 54.7MB)
+  테스트 7 〉 통과 (128.21ms, 56MB)
+  테스트 8 〉 통과 (43.80ms, 64.8MB)
+  테스트 9 〉 통과 (15.34ms, 41.5MB)
+  테스트 10 〉 통과 (16.45ms, 44MB)
+  테스트 11 〉 통과 (3563.67ms, 254MB)
+  테스트 12 〉 통과 (4018.31ms, 254MB)
+  테스트 13 〉 통과 (5175.99ms, 251MB)
+  테스트 14 〉 통과 (3826.11ms, 260MB)
+  테스트 15 〉 통과 (3035.25ms, 254MB)
+  테스트 16 〉 통과 (67.90ms, 87.3MB)
+ */
+class Node {
+  constructor(item, next = null) {
+    this.item = item;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  empty() {
+    return !this.head;
+  }
+
+  push(item) {
+    const node = new Node(item);
+    if (this.empty()) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = this.tail.next;
+    }
+    this.size += 1;
+  }
+
+  pop() {
+    if (this.head) {
+      const headItem = this.head.item;
+      let temp = new Node(0, this.head.next);
+      this.head = null;
+      this.head = temp.next;
+      temp = null;
+
+      this.size -= 1;
+
+      return headItem;
+    }
+    return -1;
+  }
+
+  front() {
+    if (this.empty()) return -1;
+    return this.head.item;
+  }
+
+  back() {
+    if (this.empty()) return -1;
+    return this.tail.item;
+  }
+}
+
 function solution(n, roads, sources, destination) {
   const adj = [...Array(n + 1)].map(() => []);
   roads.forEach((road) => {
@@ -6,30 +80,32 @@ function solution(n, roads, sources, destination) {
     adj[node2].push(node1);
   });
 
-  let min = Infinity;
-  const dfs = (pos, length = 0, visited = 0) => {
-    if (pos === destination) {
-      min = Math.min(length, min);
-      return;
-    }
+  const bfs = (start) => {
+    if (start === destination) return 0;
 
-    visited |= 1 << pos;
-    for (const child of adj[pos]) {
-      if ((visited & (1 << child)) === 0) {
-        dfs(child, length + 1, visited);
+    const distance = [...Array(n + 1).fill(-1)];
+    const q = new Queue();
+    q.push(start);
+    distance[start] = 0;
+    while (!q.empty()) {
+      const here = q.pop();
+      for (const there of adj[here]) {
+        if (distance[there] === -1) {
+          q.push(there);
+          distance[there] = distance[here] + 1;
+          if (there === destination) return distance[destination];
+        }
       }
     }
+    return distance[destination];
   };
 
   const answer = sources.reduce((acc, src) => {
     if (adj[destination].length === 0 && src !== destination) {
       acc.push(-1);
-      return acc;
+    } else {
+      acc.push(bfs(src));
     }
-
-    dfs(src);
-    acc.push(min === Infinity ? -1 : min);
-    min = Infinity;
     return acc;
   }, []);
 
